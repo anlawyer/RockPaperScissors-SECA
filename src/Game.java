d8034c06dc7a6c161ec7913e53eaba16386a70e7
Alison Lawyer <anlawyer@berkeley.edu>
2018-05-04 16:47:15 -0700

    added variables to player class, update player variables through gameplay

--- src/Game.java
+++ src/Game.java
@@ -4,35 +4,47 @@ import java.util.Scanner;
 public class Game {
     // initialize scanner receiving input
     Scanner scanner = new Scanner(System.in);
+    Player player1 = new Player();
 
     Game() {
-        // intro to game
+        // intro to game, call startGame method
         System.out.println("Welcome!");
         System.out.println("Would you like to play against the computer or a friend?");
         System.out.println("Type: computer or friend.");
         this.startGame();
     }
 
+    // call proper play method depending on user's desire
     public void startGame() {
         String determinePlayer = scanner.nextLine();
-        if (determinePlayer.toLowerCase().equals("computer")) {
-            System.out.println("You want to play: " + determinePlayer);
-            this.computerPlayer();
-        } else if (determinePlayer.toLowerCase().equals("friend")) {
-            System.out.println("friend loop " + determinePlayer);
-        } else if(determinePlayer.toLowerCase().equals("exit")) {
-            System.out.println("Thank you for playing!");
-        } else {
-            System.out.println("Would you like to play against the computer, a friend, or exit?");
-            this.startGame();
-        }
+//        try {
+            if (determinePlayer.toLowerCase().equals("computer")) {
+                System.out.println("You're about to play the: " + determinePlayer);
+                this.computerPlayer();
+            } else if (determinePlayer.toLowerCase().equals("friend")) {
+                System.out.println("You're about to play your: " + determinePlayer);
+            } else if (determinePlayer.toLowerCase().equals("exit")) {
+                System.out.println("Thanks for playing!");
+            }
+            else {
+//                throw new Exception(InputMismatchException);
+                System.out.println("Would you like to play against the computer, a friend, or exit?");
+                this.startGame();
+            }
+//        }
+//        catch () {
+//            // handle improper input
+//            System.out.println("Would you like to play against the computer, a friend, or exit?");
+//            this.startGame();
+//        }
     }
 
     public void computerPlayer () {
         System.out.println("Enter your play! Type: rock, paper, or scissors.");
 
         String playersPlay = scanner.nextLine();
-        System.out.println("You played: " + playersPlay.toLowerCase());
+        player1.play = playersPlay.toLowerCase();
+        System.out.println("You played: " + player1.play);
 
         Random randomPlay = new Random();
         int computersRand = randomPlay.nextInt(3);
@@ -48,28 +60,39 @@ public class Game {
         System.out.println("Computer played: " + computersPlay);
 
         String playAgain = "no";
-        if (computersPlay.equals(playersPlay.toLowerCase())) {
+
+        if (computersPlay.equals(player1.play)) {
             System.out.println("Tied game! Play again.");
             playAgain = "tied";
-        } else if (playersPlay.toLowerCase().equals("rock") && computersPlay.equals("paper")) {
+        } else if (player1.play.equals("rock") && computersPlay.equals("paper")) {
             System.out.println("Computer wins! Play again?");
+            player1.outcome = "lost";
             playAgain = scanner.nextLine();
-        } else if (playersPlay.toLowerCase().equals("paper") && computersPlay.equals("rock")) {
+        } else if (player1.play.equals("paper") && computersPlay.equals("rock")) {
             System.out.println("Player wins! Play again?");
+            player1.points += 1;
+            player1.outcome = "won";
             playAgain = scanner.nextLine();
-        } else if (playersPlay.toLowerCase().equals("paper") && computersPlay.equals("scissors")) {
+        } else if (player1.play.equals("paper") && computersPlay.equals("scissors")) {
             System.out.println("Computer wins! Play again?");
+            player1.outcome = "lost";
             playAgain = scanner.nextLine();
-        } else if (playersPlay.toLowerCase().equals("scissors") && computersPlay.equals("paper")) {
+        } else if (player1.play.equals("scissors") && computersPlay.equals("paper")) {
             System.out.println("Player wins! Play again?");
+            player1.outcome = "won";
+            player1.points += 1;
             playAgain = scanner.nextLine();
-        } else if (playersPlay.toLowerCase().equals("scissors") && computersPlay.equals("rock")) {
+        } else if (player1.play.equals("scissors") && computersPlay.equals("rock")) {
             System.out.println("Computer wins! Play again?");
+            player1.outcome = "lost";
             playAgain = scanner.nextLine();
-        } else if (playersPlay.toLowerCase().equals("rock") && computersPlay.equals("scissors")) {
+        } else if (player1.play.equals("rock") && computersPlay.equals("scissors")) {
             System.out.println("Player wins! Play again?");
+            player1.outcome = "won";
+            player1.points += 1;
             playAgain = scanner.nextLine();
         }
+
         switch (playAgain) {
             case "yes":
                 Game nextRound = new Game();
@@ -78,6 +101,7 @@ public class Game {
                 this.computerPlayer();
                 break;
             default:
+                System.out.println(player1.points);
                 System.out.println("Thanks for playing!");
                 break;
         }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class Player {
+    public static ArrayList<String> history;
+    public String play;
+    public int points;
+    public String outcome;
 }
